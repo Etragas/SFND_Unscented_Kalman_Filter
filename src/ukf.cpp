35a228163092ba0d5791861d495440c9adbccc56
Elias Tragas <Trags@Kathys-MacBook-Pro.local>
2021-03-02 21:24:59 -0800

    precision hard

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -90,9 +90,8 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
     }
   }
 
-  double delta_t = 0.033333;
-  // meas_package.timestamp_ - time_us_;
-  // delta_t /= 1000000;
+  double delta_t = (double)meas_package.timestamp_ - (double)time_us_;
+  delta_t /= 1000000;
   std::cout << delta_t << std::endl;
   Prediction(delta_t);
   time_us_ = meas_package.timestamp_;
